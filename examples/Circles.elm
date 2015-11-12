afd3ae821cbfa08faa082c95d4012d6ea304317e
mark2011 <mark2011@users.noreply.github.com>
2015-11-12 04:34:25 +0000

    Circles.elm - add delay to transition

--- examples/Circles.elm
+++ examples/Circles.elm
@@ -34,6 +34,7 @@ circles =
         |. num attr "cy"   150
         |. fun attr "cx"   (\_ i -> toString (25 + 50 * i))
         |. transition
+           |. delay (\_ _ -> 500)
            |. num attr "r" 25
      |- update
         |. fun attr "fill" color
